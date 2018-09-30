d616cf7fa9ed4329fbeaac810f43adf43bdf43ee
theinternetftw <theinternetftw@gmail.com>
2018-09-30 03:10:16 -0400

    Make sure banks+addrs calc and wrap correctly

--- mem.go
+++ mem.go
@@ -8,37 +8,29 @@ type mem struct {
 	rom     []byte
 
 	CartRAMPagedIn bool
-	PageRAMBank    uint16
+	PageRAMBank    uint
 
-	Page0Bank uint16
-	Page1Bank uint16
-	Page2Bank uint16
-
-	lastPageCtrl byte // for debug
-}
-
-func (m *mem) getPagingControlReg() byte {
-	return byteFromBools(
-		true,
-		true,
-		true,
-		true,
-		m.CartRAMPagedIn,
-		m.PageRAMBank == 1,
-		true,
-		true,
-	)
+	Page0Bank uint
+	Page1Bank uint
+	Page2Bank uint
 }
 
 func (m *mem) setPagingControlReg(b byte) {
-	if b != m.lastPageCtrl {
-		fmt.Printf("PageCtrl: 0x%02x last:0x%02x\n", b, m.lastPageCtrl)
-		m.lastPageCtrl = b
-	}
+	//fmt.Printf("PageCtrl: 0x%02x\n", b)
 	m.CartRAMPagedIn = b&8 > 0
-	m.PageRAMBank = uint16((b & 4) >> 2)
-	//assert(b&0x80 == 0, "dev machine ROM Write mode not implemented")
-	//assert(b&0x08 == 0, "cart RAM over internal RAM mode not yet implemented")
+	m.PageRAMBank = uint((b & 4) >> 2)
+	//fmt.Printf("\tRAM Paged In: %v\n", m.CartRAMPagedIn)
+	//fmt.Printf("\tRAM Bank: %v\n", m.PageRAMBank)
+	assert(b&0x10 == 0, "cart RAM over internal RAM mode not yet implemented")
+}
+
+func (m *mem) wrapROMBankNum(bankNum byte) byte {
+	// should always be a power of two - 1
+	if len(m.rom) <= 16*1024 {
+		return 0
+	}
+	maxBankNum := byte((len(m.rom) / (16 * 1024)) - 1)
+	return bankNum & maxBankNum
 }
 
 func (emu *emuState) read(addr uint16) byte {
@@ -48,17 +40,25 @@ func (emu *emuState) read(addr uint16) byte {
 		val = m.rom[addr]
 	} else if addr < 0x4000 {
 		bank := m.Page0Bank
-		val = m.rom[bank*0x4000+addr]
+		computedAddr := bank*0x4000 + uint(addr)
+		//fmt.Println("ROM BANK", bank, "addr", addr, "computed", computedAddr)
+		val = m.rom[computedAddr]
 	} else if addr < 0x8000 {
 		bank := m.Page1Bank
-		val = m.rom[bank*0x4000+(addr-0x4000)]
+		computedAddr := bank*0x4000 + uint(addr-0x4000)
+		//fmt.Println("ROM BANK", bank, "addr", addr, "computed", computedAddr)
+		val = m.rom[computedAddr]
 	} else if addr < 0xc000 {
 		if m.CartRAMPagedIn {
 			bank := m.PageRAMBank
-			val = m.CartRAM[bank*0x4000+(addr-0x8000)]
+			computedAddr := bank*0x4000 + uint(addr-0x8000)
+			//fmt.Println("RAM BANK", bank, "addr", addr, "computed", computedAddr)
+			val = m.CartRAM[computedAddr]
 		} else {
 			bank := m.Page2Bank
-			val = m.rom[bank*0x4000+(addr-0x8000)]
+			computedAddr := bank*0x4000 + uint(addr-0x8000)
+			//fmt.Println("ROM BANK", bank, "addr", addr, "computed", computedAddr)
+			val = m.rom[computedAddr]
 		}
 	} else if addr < 0xe000 {
 		val = m.RAM[addr-0xc000]
@@ -75,8 +75,8 @@ func (emu *emuState) write(addr uint16, val byte) {
 	} else if addr < 0xc000 {
 		if m.CartRAMPagedIn {
 			bank := m.PageRAMBank
-			m.CartRAM[bank*0x4000+(addr-0x8000)] = val
-		} else {
+			computedAddr := bank*0x4000 + uint(addr-0x8000)
+			m.CartRAM[computedAddr] = val
 		}
 	} else if addr < 0xe000 {
 		m.RAM[addr-0xc000] = val
@@ -86,11 +86,14 @@ func (emu *emuState) write(addr uint16, val byte) {
 		if addr == 0xfffc {
 			m.setPagingControlReg(val)
 		} else if addr == 0xfffd {
-			m.Page0Bank = uint16(val)
+			m.Page0Bank = uint(m.wrapROMBankNum(val))
+			//fmt.Println("set bank0:", m.Page0Bank)
 		} else if addr == 0xfffe {
-			m.Page1Bank = uint16(val)
+			m.Page1Bank = uint(m.wrapROMBankNum(val))
+			//fmt.Println("set bank1:", m.Page1Bank)
 		} else if addr == 0xffff {
-			m.Page2Bank = uint16(val)
+			m.Page2Bank = uint(m.wrapROMBankNum(val))
+			//fmt.Println("set bank2:", m.Page2Bank)
 		}
 	}
 }
@@ -98,15 +101,20 @@ func (emu *emuState) write(addr uint16, val byte) {
 func (emu *emuState) in(addr uint16) byte {
 	addr &= 0xff // sms ignores upper byte
 	var val byte
-	if addr <= 0x3f {
+	if addr < 0x40 {
 		val = 0xff // right for SMS2, SMS has weird bus stuff
-	} else if addr >= 0x40 && addr <= 0x7f {
-		errOut(fmt.Sprintf("IN command not yet impld 0x%02x", addr))
-	} else if addr >= 0x80 && addr <= 0xbf {
+	} else if addr < 0x80 {
+		if addr&1 == 0 {
+			val = emu.VDP.readVCounter()
+		} else {
+			// TODO: add latch, only update value when lightgun pin changes
+			val = emu.VDP.readHCounter()
+		}
+	} else if addr < 0xc0 {
 		if addr&1 == 0 {
 			val = emu.VDP.readDataPort()
 		} else {
-			errOut(fmt.Sprintf("IN command not yet impld 0x%02x", addr))
+			val = emu.VDP.readControlPort()
 		}
 	} else { // >= 0xc0
 		if addr&1 == 0 {
@@ -115,26 +123,35 @@ func (emu *emuState) in(addr uint16) byte {
 			val = emu.readJoyReg1()
 		}
 	}
+	//fmt.Printf("IN: %04x, %02x\n", addr, val)
+	//fmt.Printf("got IN: 0x%02x = 0x%02x\n", addr, val)
 	return val
 }
 
 func (emu *emuState) out(addr uint16, val byte) {
 	addr &= 0xff // sms ignores upper byte
-	if addr >= 0x40 && addr <= 0x7f {
+	//fmt.Printf("got OUT: 0x%02x, 0x%02x\n", addr, val)
+	if addr < 0x40 {
+		if addr&1 == 0 {
+			errOut(fmt.Sprintf("OUT command not yet impld 0x%02x, 0x%02x", addr, val))
+		} else {
+			emu.setIOControlReg(val)
+		}
+	} else if addr < 0x80 {
 		if addr&1 == 0 {
 			//errOut(fmt.Sprintf("sound not yet impled, got OUT 0x%02x, 0x%02x", addr, val))
 		} else {
 			//errOut(fmt.Sprintf("sound not yet impled, got OUT 0x%02x, 0x%02x", addr, val))
 		}
-	} else if addr >= 0x80 && addr <= 0xbf {
+	} else if addr < 0xc0 {
 		if addr&1 == 0 {
+			//fmt.Printf("write data port 0x%02x\n", val)
 			emu.VDP.writeDataPort(val)
 		} else {
+			//fmt.Printf("write control port 0x%02x\n", val)
 			emu.VDP.writeControlPort(val)
 		}
-	} else if addr >= 0xc0 {
+	} else { // >= 0xc0
 		// NOP: writes == old SG-3000 keyboard ports that don't matter to sms
-	} else {
-		errOut(fmt.Sprintf("OUT command not yet impld 0x%02x, 0x%02x", addr, val))
 	}
 }
